718761850e0af5bcd91ef9a718c1d8bce76a7305
verenddayz <verenddayz@gmail.com>
2021-10-18 13:15:01 +0700

    update templating

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 //$config['base_url'] = 'http://192.168.10.127/ybs-public/';
 //$config['base_url'] = 'http://192.168.43.60/ybs-public/';
-$config['base_url'] = 'http://localhost/indipass/report/';
+$config['base_url'] = 'http://localhost/Loyalty-Segment2/';
 //$config['base_url'] = 'http://localhost/ybs_online/ybs/';
 
 /*
